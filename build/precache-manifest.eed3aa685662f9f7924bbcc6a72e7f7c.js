5d41a222e017ae01ac57b080ac9481ba2951f178
danyparc <danyparc@gmail.com>
2019-06-08 14:13:13 -0500

    preparado para producción

---
+++ build/precache-manifest.eed3aa685662f9f7924bbcc6a72e7f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76690a8ed57e880e490fc6ea9b4146e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc40b23974b5f780c253",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b48c82d1d030e91ae308",
+    "url": "/static/js/2.2c0a3059.chunk.js"
+  },
+  {
+    "revision": "dc40b23974b5f780c253",
+    "url": "/static/js/main.6589d43b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
